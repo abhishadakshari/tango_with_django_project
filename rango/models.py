4b54251833ec0b716d6c5af1360dd501e6b63a2c
Abhishek Shadakshari <2772079s@student.gla.ac.uk>
2024-01-26 10:36:31 +0000

    ForeignKey now working

--- rango/models.py
+++ rango/models.py
@@ -3,18 +3,14 @@ from django.db import models
 
 class Category(models.Model):
     name = models.CharField(max_length=128, unique=True)
-
     def __str__(self):
         return self.name
-
-
 class Page(models.Model):
-    category = models.ForeginKey(Category, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
     title = models.CharField(max_length=128)
     url = models.URLField()
     views = models.IntegerField(default=0)
 
     def __str__(self):
         return self.title
-
 
