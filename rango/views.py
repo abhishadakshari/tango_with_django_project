ef14371f9c5ad5a6c882a6e2aaab54225e584979
Abhishek Shadakshari <2772079s@student.gla.ac.uk>
2024-01-30 14:57:10 +0000

    Category template

--- rango/views.py
+++ rango/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-from rango.models import Category
+from rango.models import Category, Page
 from django.http import HttpResponse
+
 
 def index(request):
     category_list = Category.objects.order_by('-likes')[:5]
@@ -14,3 +15,21 @@ def index(request):
 def about(request):
     context_dict = {'boldmessage': 'This tutorial has been put together by Abhishek Shadakshari'}
     return render(request, 'rango/about.html', context = context_dict)
+
+def show_category(request, category_name_slug):
+
+    context_dict = {}
+
+    try:
+        category = Category.objects.get(slug=category_name_slug)
+        pages = Page.objects.filter(category=category)
+
+        context_dict['pages'] = pages
+        context_dict['category'] = category
+
+    except Category.DoesNotExist:
+
+        context_dict['pages'] = None
+        context_dict['category'] = None
+
+    return render(request, 'rango/category.html', context = context_dict)
